7556667bf7796e85199df24fb9473c3ed0ba4bd2
toshi468 <fu.nobuchika0809@gmail.com>
2025-10-28 02:51:41 +0900

    API開発の環境ファイルをつくる(ここにapiを書いていく)

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -1,8 +1,1 @@
 import { NextRequest } from 'next/server';
-
-export async function GET(req: NextRequest) {
-  return new Response(JSON.stringify({ message: "Hello from API!" }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
-}
