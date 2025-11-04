19c84063ac8423e9be87346d47442ea9df3effee
toshi468 <fu.nobuchika0809@gmail.com>
2025-11-04 19:59:30 +0900

    localStorageを使用して更新しても履歴が残るように。

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,10 @@ export default function MainPage() {
   const [todos, setTodos] = useState<Todo[]>([]);
   useEffect(() => {
     const fetchTodos = async () => {
+    const stored = localStorage.getItem("todos");
+    if (stored) {
+      setTodos(JSON.parse(stored));
+    }else{
       try {
         const res = await fetch("/api/todos");
         if (!res.ok) {
@@ -17,9 +21,14 @@ export default function MainPage() {
       } catch (error) {
         console.error("Error fetching todos:", error);
       }
+    }
     };
     fetchTodos();
   }, []);
+
+  useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
   return (
     <div className="h-screen flex flex-col">
       <h2 className="flex justify-center mt-20 text-6xl">Todoリスト</h2>
